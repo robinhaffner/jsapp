6944ac0eaa7fc9fc4c0e230daadd1e436f4b0317
berryny <judesire@yahoo.com>
2014-07-06 10:25:27 -0400

    fixes

--- static/js/site.js
+++ static/js/site.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
 	$('[data-toggle="offcanvas"]').click(function (event) {
 		event.preventDefault();
 		if ($("#sidebar").position().left < -1.0) {
+			$(".row-offcanvas .sidebar-offcanvas").css('visibility', 'visible');
 			$('.row-offcanvas').toggleClass('active');
 		} else {
+			$(".row-offcanvas .sidebar-offcanvas").css('visibility', 'hidden');
 			$('.row-offcanvas').removeClass('active');
 		}
 	});
