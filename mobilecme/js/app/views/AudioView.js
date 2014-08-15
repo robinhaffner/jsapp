8c44ca538e1bd0d81f3fa4b393621e769229a440
robinhaffner <robin.haffner@me.com>
2014-08-15 14:39:29 -0400

    Update AudioView.js
    fixed click state for icon and audio play behavior

--- mobilecme/js/app/views/AudioView.js
+++ mobilecme/js/app/views/AudioView.js
@@ -35,13 +35,13 @@ define(function (require) {
             this.pageAudioPlayer();
         };
 
-        this.pageAudioPlayer = function () {
+ this.pageAudioPlayer = function () {
             var apContainer = $(_getaudio).find('.audioplay'),
                 getAudioURL = apContainer.data('audiourl'),
                 getAutoPlay = apContainer.data('autoplay');
             var _playState = Cookies.get("cmeaudio"); //check to see if the user has paused the audio
             if (_playState == undefined) { //if the audi state cookie has not been set - initialize it here.
-                Cookies.set('cmeaudio','play');
+                if (getAutoPlay) {Cookies.set('cmeaudio','play') }else{ Cookies.set('cmeaudio','pause')};
             }
 
             if (apContainer.length == 1 ){ // set up the jwPlayer for audio
@@ -49,29 +49,28 @@ define(function (require) {
                     file: getAudioURL,
                     height: '0',
                     width: '0'
-            
                 });
+	
                 console.log("_playState",_playState);
-                if (_playState != "pause" || _playState == undefined){ //set the icon state and play or pause the video
-                        if (getAutoPlay ){
+                if (_playState != "pause" && getAutoPlay){ //set the icon state and play or pause the video
                             jwplayer('audioPlayer').play(true);
-                            $('.icon-sound').removeClass('none pause');
+					    	$('.icon-sound').removeClass('none pause');
                             $('.icon-sound').addClass('on');
-                            $(apContainer).data('cmeaudio','play');
+                           // $(apContainer).data('cmeaudio','play');
                             console.log('Play!');
-                        }else if (!getAutoPlay){
+                 }else {			
                             jwplayer('audioPlayer').play(false);
-                            $('.icon-sound').removeClass('none on');
+					   		$('.icon-sound').removeClass('none on');
                             $('.icon-sound').addClass('pause');
-                            $(apContainer).data('cmeaudio','pause');
+                            //$(apContainer).data('cmeaudio','pause');
                             console.log('Pause!');
-                        }else{
-                            $('.icon-sound').addClass('none');
-                        }
-                    }
+                 }
+             
 
             } else { //No video for this page
+				$('.icon-sound').removeClass('pause on');
                 $('.icon-sound').addClass('none');
+
                 return
             }
         };
@@ -79,22 +78,21 @@ define(function (require) {
 
         $(document).on('click',".icon-sound", function(event) {
             var _audioPlayer = jwplayer('audioPlayer');
-            var apContainer = $(_getaudio).find('.audioplay');
+           // var apContainer = $(_getaudio).find('.audioplay');
             var getController = $(_getaudio).find('.audioplay').data('cmeaudio');
 
-            var _playState = Cookies.get("cmeaudio");
-            console.log("click",_playState);
+            var _playState =  _audioPlayer.getState();//Cookies.get("cmeaudio");
+			_audioPlayer.play();
+
             
-            if (_playState == 'pause') {
+            if (_playState == 'PAUSED' || _playState == 'IDLE') {
                 Cookies.set('cmeaudio','play');
-                $(apContainer).data('cmeaudio','play');
-                _audioPlayer.play(true);
+              //  $(apContainer).data('cmeaudio','play');
                 $('.icon-sound').removeClass('none pause');
                 $('.icon-sound').addClass('on');
 
-            } else if (_playState == 'play') {
-                _audioPlayer.pause(true);
-                $(apContainer).data('cmeaudio','pause');
+            } else if (_playState == 'PLAYING') {
+             //   $(apContainer).data('cmeaudio','pause');
                 $('.icon-sound').removeClass('none on');
                 $('.icon-sound').addClass('pause');
                 Cookies.set('cmeaudio','pause');
@@ -102,6 +100,8 @@ define(function (require) {
             } else { return }
                 
         });
+
+
 
 
         this.initialize();
